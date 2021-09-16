f0d2920f552c28b141185314f05313463bbd42cb
Enes Fehmi Manan <88631980+enesmanan@users.noreply.github.com>
2021-09-16 17:08:42 +0300

    Add files via upload

---
+++ if-else-if/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+            if (time >= 5 && time <= 12)
+            {
+                Console.WriteLine("Gunaydın.");
+            }
+            else if (time <= 19)
+            {
+                Console.WriteLine("Iyi gunler.");
+            }
+            else
+            {
+                Console.WriteLine("Iyi geceler.");
+            }
+
+            string sonuc = time < 19 ? "Iyi gunler." : "Iyi geceler.";
+
+            sonuc = time >= 5 && time <= 12 ? "Günaydın" : time <= 19 ? "Iyi gunler." : "Iyi geceler";
+
+            Console.WriteLine(sonuc);
+        }
+    }
+}
