1a2a60c2418eb1ad81b7fa660f3e06f1ff273be1
Enes Fehmi Manan <88631980+enesmanan@users.noreply.github.com>
2021-09-16 17:03:41 +0300

    Add files via upload

---
+++ First Folder/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("İsminiz Lutfen!");
+            string name = Console.ReadLine();
+            Console.WriteLine("Soyadınız Lutfen!");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Yasınız Lutfen!");
+            string yas = Console.ReadLine();
+
+            Console.WriteLine("Hosgeldin!" + " " + name + " " + surname + " " + yas);
+            Console.ReadKey();
+        }
+    }
+}
